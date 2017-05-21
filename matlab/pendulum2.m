42854d8cab3d9f79e390259057af9f4411105365
dagothar <dagothar@gmail.com>
2017-05-21 12:13:30 +0200

    ...

--- matlab/pendulum2.m
+++ matlab/pendulum2.m
@@ -2,7 +2,6 @@ clc; clear all; close all;
 
 %% Symbolic variables
 syms t m1 m2 l1 l2 g tau1 tau2 th1(t) th2(t)
-syms q1 q2 dq1 dq2 d2q1 d2q2 
 
 dth1 = diff(th1, t);
 dth2 = diff(th2, t);
@@ -27,36 +26,40 @@ v2sq = vx2^2 + vy2^2;
 
 T = simplify(0.5 * m1 * v1sq + 0.5 * m2 * v2sq)
 V = simplify(-m1 * g * y1 - m2 * g * y2)
-L = simplify(T - V);
-
-T = subs(T, {th1, dth1, th2, dth2}, {q1, dq1, q2, dq2})
-V = subs(V, {th1, dth1, th2, dth2}, {q1, dq1, q2, dq2})
-L = subs(L, {th1, dth1, th2, dth2}, {q1, dq1, q2, dq2})
+L = simplify(T - V)
 
 %% Lagrange derivatives
+syms q1 dq1 d2q1 q2 dq2 d2q2
 
-d_L_dq1 = diff(L, dq1);
-d_L_dq2 = diff(L, dq2);
-d_L_q1 = diff(L, q1);
-d_L_q2 = diff(L, q2);
+% Manually substitute...
+L = (l1^2*m1*dq1^2)/2 + (l1^2*m2*dq1^2)/2 + (l2^2*m2*dq2^2)/2 + g*m2*(l1*cos(q1)) + l2*cos(q2) + g*l1*m1*cos(q1) + l1*l2*m2*cos(q1 - q2)*dq1*dq2;
 
-syms Q1(t) Q2(t) dQ1(t) dQ2(t)
-d_L_dq1 = subs(d_L_dq1, {q1, dq1, q2, dq2}, {Q1, dQ1, Q2, dQ2});
-d_L_dq2 = subs(d_L_dq2, {q1, dq1, q2, dq2}, {Q1, dQ1, Q2, dQ2});
+d_L_dq1 = diff(L, dq1)
+d_L_q1 = diff(L, q1)
 
-d_L_t1 = diff(d_L_dq1, t);
-d_L_t2 = diff(d_L_dq2, t);
+d_L_dq2 = diff(L, dq2)
+d_L_q2 = diff(L, q2)
 
-d_L_t1 = subs(d_L_t1, {Q1, dQ1, Q2, dQ2}, {q1, dq1, q2, dq2});
-d_L_t2 = subs(d_L_t2, {Q1, dQ1, Q2, dQ2}, {q1, dq1, q2, dq2});
+syms Q1(t) Q2(t)
+dQ1 = diff(Q1, t);
+dQ2 = diff(Q2, t);
+d2Q1 = diff(Q1, t);
+d2Q2 = diff(Q2, t);
 
+d_L_dq1 = subs(d_L_dq1, {q1, dq1, q2, dq2}, {Q1, dQ1, Q2, dQ2})
+d_L_dq2 = subs(d_L_dq2, {q1, dq1, q2, dq2}, {Q1, dQ1, Q2, dQ2})
+
+d_L_t1 = diff(d_L_dq1, t)
+d_L_t2 = diff(d_L_dq2, t)
+
+% Manually substitute...
+d_L_t1 = l1^2*m1*d2q1 + l1^2*m2*d2q1 + l1*l2*m2*cos(q1 - q2)*d2q2 - l1*l2*m2*sin(q1 - q2)*dq2*(dq1 - dq2);
+d_L_t2 = l2^2*m2*d2q2 + l1*l2*m2*cos(q1 - q2)*d2q1 - l1*l2*m2*sin(q1 - q2)*dq1*(dq1 - dq2);
+
+%% Euler-Lagrange equations
 e1 = d_L_t1 - d_L_q1
 e2 = d_L_t2 - d_L_q2
 
-e1 = subs(e1, {th1, dth1, d2th1, th2, dth2, d2th2}, {q1, dq1, d2q1, q2, dq2, d2q2});
-e2 = subs(e2, {th1, dth1, d2th1, th2, dth2, d2th2}, {q1, dq1, d2q1, q2, dq2, d2q2});
-
-%% Euler-Lagrange equations
 eqns = [
     e1 == 0
     e2 == 0
