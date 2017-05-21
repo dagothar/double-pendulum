a4eaf1ec41c8145f3355250ac6a08c88eda31618
dagothar <dagothar@gmail.com>
2017-05-21 11:09:54 +0200

    ...

--- matlab/pendulum.m
+++ matlab/pendulum.m
@@ -25,8 +25,8 @@ v1sq = vx1^2 + vy1^2;
 v2sq = vx2^2 + vy2^2;
 
 T = 0.5 * m1 * v1sq + 0.5 * m2 * v2sq
-U = -m1 * g * y1 - m2 * g * y2
-L = T - U
+V = -m1 * g * y1 - m2 * g * y2
+L = T - V
 
 %% Lagrange derivatives
 syms q1 q2 dq1 dq2 d2q1 d2q2 
@@ -36,8 +36,10 @@ d_L_dq2 = diff(subs(L, dth2, dq2), dq2);
 d_L_q1 = diff(subs(L, th1, q1), q1);
 d_L_q2 = diff(subs(L, th2, q2), q2);
 
-d_L_dq1 = subs(d_L_dq1, {q1, dq1, q2, dq2}, {th1, dth1, th2, dth2});
-d_L_dq2 = subs(d_L_dq2, {q1, dq1, q2, dq2}, {th1, dth1, th2, dth2});
+d_L_dq1 = subs(d_L_dq1, dq1, dth1);
+d_L_dq2 = subs(d_L_dq2, dq2, dth2);
+d_L_q1 = subs(d_L_q1, q1, th1);
+d_L_q2 = subs(d_L_q2, q2, th2);
 
 d_L_t1 = diff(d_L_dq1, t);
 d_L_t2 = diff(d_L_dq2, t);
@@ -45,6 +47,8 @@ d_L_t2 = diff(d_L_dq2, t);
 e1 = d_L_t1 - d_L_q1;
 e2 = d_L_t2 - d_L_q2;
 
+T = subs(T, {th1, dth1, d2th1, th2, dth2, d2th2}, {q1, dq1, d2q1, q2, dq2, d2q2});
+V = subs(V, {th1, dth1, d2th1, th2, dth2, d2th2}, {q1, dq1, d2q1, q2, dq2, d2q2});
 e1 = subs(e1, {th1, dth1, d2th1, th2, dth2, d2th2}, {q1, dq1, d2q1, q2, dq2, d2q2});
 e2 = subs(e2, {th1, dth1, d2th1, th2, dth2, d2th2}, {q1, dq1, d2q1, q2, dq2, d2q2});
 
@@ -57,16 +61,14 @@ eqns = [
 %% Solve equations
 [d2q1, d2q2] = solve(eqns, [d2q1 d2q2]);
 T = simplify(T)
-U = simplify(U)
+V = simplify(V)
 d2q1 = simplify(d2q1)
 d2q2 = simplify(d2q2)
 
 %% Save to file
 fid = fopen('equations.txt', 'wt');
-T = subs(T, {th1, dth1, th2, dth2}, {q1 dq1, q2, dq2});
-U = subs(U, {th1, dth1, th2, dth2}, {q1 dq1, q2, dq2});
+fprintf(fid, 'T = %s\n', char(T));
+fprintf(fid, 'V = %s\n', char(V));
 fprintf(fid, 'd2q1 = %s\n', char(d2q1));
 fprintf(fid, 'd2q2 = %s\n', char(d2q2));
-fprintf(fid, 'T = %s\n', char(T));
-fprintf(fid, 'U = %s\n', char(U));
 fclose(fid);
